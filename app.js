3cb23a1202da5b7064e40873f9aee0049aff6a71
Ubuntu <ubuntu@ip-172-31-19-201.ap-northeast-2.compute.internal>
2018-11-09 11:33:18 +0000

    로그인, 회원가입 버튼

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
-var server = app.listen(3000, function(){
-	console.log("Express server has started on port 3000")
+var server = app.listen(3516, function(){
+	console.log("Express server has started on port 3516")
 })
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
 
 app.get('/',function(req, res){
-	res.send('hello world');
+	res.render('user/login.html');
+});
+app.get('/signup', function(req, res){
+	res.render('user/signup.html');
 });
 
